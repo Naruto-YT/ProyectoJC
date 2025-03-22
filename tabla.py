297451f276d45b4f2b55b595c10f63d86a1bd8d7
Hermogenes Palacio <123488893+Naruto-YT@users.noreply.github.com>
2025-03-22 07:37:47 -0500

    Create tabla.py
    tabla

---
+++ tabla.py
@@ -0,0 +1,8 @@
+def obtener_tabla():
+    return """Categoría       | Unidades Disponibles
+----------------------------------------
+Distancia      | metros (m), pies (ft), kilómetros (km), millas (mi)
+Temperatura    | celsius (°C), fahrenheit (°F), kelvin (K)
+Masa           | kilogramos (kg), libras (lb), gramos (g), onzas (oz)
+Velocidad      | kilómetros/h (km/h), metros/s (m/s), millas/h (mph)
+"""
